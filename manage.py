2c42ec1e8f13b512d0f482bc31bdde256bcfde34
Tejas Waghamare <tejaswaghamare121@gmail.com>
2025-04-25 11:30:11 +0530

    second

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    """hello"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Recommendationsystem.settings')
     try:
         from django.core.management import execute_from_command_line
